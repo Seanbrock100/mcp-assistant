4ead61e1ca796934d1dfa133792a097a2f6824fc
Seanbrock100 <47324709+Seanbrock100@users.noreply.github.com>
2025-05-22 20:05:51 +0100

    Update strava_oauth_flask_server.py

--- strava_oauth_flask_server.py
+++ strava_oauth_flask_server.py
@@ -42,4 +42,4 @@ def exchange_token():
         return f"<h2>Error:</h2><pre>{response.text}</pre>", 400
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
