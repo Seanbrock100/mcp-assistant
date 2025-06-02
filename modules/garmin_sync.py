6fbbe7013387ee999cab5a5839578cbf7bb01639
Sean Brock <seanbrock100@gmail.com>
2025-06-02 09:50:09 +0100

    Organise files into modules and logic folders

---
+++ modules/garmin_sync.py
@@ -0,0 +1,8 @@
+def get_daily_garmin_data():
+    # TODO: Fetch today's data from Garmin
+    return {
+        "sleep_score": 85,
+        "resting_hr": 48,
+        "activities": [],
+        "hrv": 72
+    }
---
+++ modules/runna_scraper.py
@@ -0,0 +1,7 @@
+def get_today_plan():
+    # TODO: Parse calendar or API to fetch today's plan
+    return {
+        "session": "Easy Run",
+        "duration": "45 mins",
+        "intensity": "low"
+    }
